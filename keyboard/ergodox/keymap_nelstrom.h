23ac24c1d1b480fdafcda969d0ab6b584dff49db
Drew Neil <andrew.jr.neil@gmail.com>
2014-10-03 17:25:36 +0100

    Set hjkl as arrow keys on layer 1
    Top tip:
      Hold down the space key (right thumb) then:
        h - left
        j - down
        k - up
        l - right

--- keyboard/ergodox/keymap_nelstrom.h
+++ keyboard/ergodox/keymap_nelstrom.h
@@ -33,7 +33,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              F12, F6,  F7,  F8,  F9,  F10, TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-                  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                  LEFT,DOWN,UP  ,RGHT,TRNS,TRNS,
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
